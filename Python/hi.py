213ec2b2bf5bad5e8ebeddda6be3e5217b1df2d7
Eve <culinevelio@gmail.com>
2024-01-23 08:30:36 -0600

    Mi primer Commit

---
+++ Python/hi.py
@@ -0,0 +1,39 @@
+print("Hola Mundo")
+
+x = 1442
+y = 56.78
+z = True
+k= False
+
+a = [1, 2, 3, False]
+print(x, y, a, sep="***", end= "\n\n\n")
+
+#HI WHAT DO U DOING 
+
+'''
+Condicionales
+'''
+
+#IF
+#ELSE
+
+if(z != k): 
+    print('Distinto')
+
+    if(x>y):
+        pass
+    elif(z):
+        print(z)
+    else: 
+        print('No coiciden')
+        var = 50 if a> 5 else 20 
+        print(var)
+
+        '''
+        
+        
+
+        '''
+arr= [1, 2, 3, 4, 5]
+for i in arr:
+ print(i)
