6c8ae1f3b95a8daecf09e09309c56a71238a507a
Eve <culinevelio@gmail.com>
2024-02-09 08:25:25 -0600

    Lista Doblemente Ligada & Lista Circular

---
+++ Python/Nodo2.py
@@ -0,0 +1,14 @@
+class Nodo:
+    '''Creacion de Nodo'''
+
+    def __init__(self, val: int = None):
+        '''Inicializacion de clase'''
+        self.val = val
+        self.next = None
+        self.prev = None  # Nuevo enlace para el nodo anterior
+
+    def __str__(self) -> str:
+        if self.val:
+            return f'Valor del Nodo = {self.val}'
+
+        return f'El nodo no tiene valor'
