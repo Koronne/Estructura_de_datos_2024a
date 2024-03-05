67c0421e160c50a2cd4e98c57f3d9063234ba862
Eve <culinevelio@gmail.com>
2024-03-05 08:12:13 -0600

    Modificacion

--- ListaDinamicaCompleta_base.py
+++ ListaDinamicaCompleta_base.py
@@ -11,24 +11,20 @@ import ctypes
 '''
 
 class ListaDinamica:
-    def __init__(self, capacidad) -> None:
+    def __init__(self) -> None:
         ''' Inicializar un arreglo vacío '''
         self.__n = 0                          # Elementos actuales
-        self.__c = capacidad                          # Capacidad inicial por defecto
+        self.__c = 1                          # Capacidad inicial por defecto
         self.__A = self.__init_arr(self.__c)  # Arreglo contenedor
     
     def __Check_resize(decrease_func):
         ''' Decorador para revisar relación elementos/capacidad '''
         def inner(self, *args):
-            if not (len(self)):
-                raise IndexError('La lista esta vacia')
-            
             val = decrease_func(self, *args)
 
-            ''' Manejar resize aquí '''
-            if self.__n <= self.__c // 4:
+            if self.__n == self.__c // 4:
                 self.__resize(self.__c // 2)
-
+            
             return val
 
         return inner
@@ -53,33 +49,30 @@ class ListaDinamica:
         for i in range(self.__n):
             if self.__A[i] == obj:
                 return True
-                
+
         return False
     
     def __eq__(self, l:ListaDinamica) -> bool:
         ''' Revisa si dos listas son iguales '''
-        if len(self) != len(l):
+        if len(self.__A) != len(l):
             return False
-        
 
-        for i in range(len(self)):
+        for i in range(self.__n):
             if self.__A[i] != l[i]:
                 return False
-            
+
         return True
     
     def __ne__(self, l:ListaDinamica) -> bool:
         ''' Revisa si dos listas no son iguales '''
         return not self.__eq__(l)
-
     
     @__Check_resize
     def __delitem__(self, k) -> None:
         ''' Elimina un elemento de la lista '''
-
-        for i in range(k , len(self)-1):
+        for i in range(k, len(self)-1):
             self.__A[i] = self.__A[i+1]
-
+        
         self.__n -= 1
 
     def __add__(self, l:ListaDinamica) -> ListaDinamica:
@@ -88,21 +81,22 @@ class ListaDinamica:
 
         for i in range(len(self)):
             holder.append(self.__A[i])
-
+        
         for i in range(len(l)):
             holder.append(l[i])
-
+        
         return holder
     
     def __iadd__(self, l:ListaDinamica) -> ListaDinamica:
         ''' Agrega elementos al final de la lista (in-place) '''
         for i in range(len(l)):
             self.append(l[i])
-        return  self
+
+        return self
     
     def __str__(self) -> str:
         ''' Retorna la representación en cadena de la lista '''
-        return '[' + ', '.join(str(self.__A[i]) for i in range(len(self))) + ']'
+        return '[' + ', '.join(str(self.__A[i]) for i in range(self.__n)) + ']'
 
     def append(self, obj) -> None:
         ''' Añade un elemento al final de la lista '''
@@ -114,84 +108,87 @@ class ListaDinamica:
     
     def count(self, obj) -> None:
         ''' Cuenta las instancias de un objeto dentro de la lista '''
-        contador = 0 
+        counter = 0
 
-        for i in range(len(self)):
+        for i in range(self.__n):
             if self.__A[i] == obj:
-                contador += 1
+                counter += 1
 
-        return contador
+        return counter
     
     def index(self, obj) -> int:
         ''' Regresa la posición de la primera instancia de un
         objeto en una lista '''
-        for i in range(len(self)):
+        for i in range(self.__n):
             if self.__A[i] == obj:
-                return i 
-            
-        raise ValueError[f'El objeto {obj} no esta en la lista']
+                return i
+        
+        raise ValueError(f'{obj} no se encuentra en la lista')
 
     def insert(self, k, obj) -> None:
         ''' Inserta un objeto en la posición k de la lista '''
         if self.__n == self.__c:
             self.__resize(2 * self.__c)
 
-        k  += len(self) if k < 0 else 0
+        k += len(self) if k < 0 else 0
 
-        for i in range(len(self), k, -1):
-            self.__A[i] = self.__A[i - 1]
+        for i in range(self.__n, k, -1):
+            self.__A[i] = self.__A[i-1]
         
         self.__A[k] = obj
         self.__n += 1
-
     
     @__Check_resize
     def pop(self, k=None) -> object:
         ''' Retorna el elemento k de una lista y lo elimina de la misma '''
-        if not k and k != 0:
-            k = self.__n - 1
-
-        if not (0 <= k < len(self)):
-            raise IndexError('Índice fuera de rango')
-
+        if not k:
+            self.__n -= 1
+            return self.__A[self.__n]
+        
         val = self.__A[k]
+        k += len(self) if k < 0 else 0
 
-        for i in range(k, len(self) - 1):
-            self.__A[i] = self.__A[i + 1]
-
+        for i in range(k, len(self)-1):
+            self.__A[i] = self.__A[i+1]
+        
         self.__n -= 1
         return val
 
     def remove(self, obj) -> None:
         ''' Elimina la primera instancia de un objeto en la lista 
         Nota: depende de la implementación de == de cada elemento '''
-        idx = self.index(obj)
-        self.pop(idx)
+        i = 0
 
+        while i != len(self):
+            if self.__A[i] == obj:
+                self.pop(i)
+                return
+            i += 1
+
+        raise ValueError(f'{obj} no se encuentra en la lista')
 
     def extend(self, l:ListaDinamica) -> None:
         ''' Agrega elementos al final de la lista '''
-        self += l
-        
+        self.__iadd__(l)
 
     def reverse(self) -> None:
         ''' Invierte los elementos de la lista '''
-        for i in range(len(self) // 2):
-            self.__A[i], self.__A[self.__n -i -1] = \
-                self.__A[self.__n -i -1], self.__A[i] 
+        list_len = len(self)
 
-
+        for i in range(list_len//2):
+            self.__A[i], self.__A[list_len - 1 - i] \
+                = self.__A[list_len - 1 - i], self.__A[i]
     
     def sort(self) -> None:
         ''' Ordena los elementos de la lista en forma ascendente (InsertionSort)'''
-        for i in range(1, self.__n):
-            cur = self.__A[i]
-            j = i 
+        for k in range(1, len(self)):
+            cur = self.__A[k]
+            j = k
 
-            while j > 0 and self.__A[j - 1] > cur:
-                self.__A[j] = self.__A[j - 1]
+            while j > 0 and self.__A[j-1] > cur:
+                self.__A[j] = self.__A[j-1]
                 j -= 1
-        
+            
             self.__A[j] = cur
 
     def __resize(self, c) -> None:
@@ -211,70 +208,9 @@ class ListaDinamica:
 if __name__ == '__main__':
     ''' Pruebas de la clase '''
     ld = ListaDinamica()
-
-    # Agregar elementos a la lista
-    ld.append(1)
-    ld.append(2)
-    ld.append(3)
-    ld.append(4)
-    ld.append(5)
-
-    # Imprimir la lis
-    print(ld)
-
-    #Probando el metodo de len
-    print('Len = ', len(ld))
-
-    # Probando el metodo de pop
-    ld.pop()
-    print('Después de Pop():\n', ld)
-
-    # Probando el metodo de insert
-    ld.insert(1, 10)
-    print('Después de insert(2, 10):\n', ld)
-
-    # Probando el metodo de remove
-    ld.remove(3)
-    print('Después de remove(3):\n', ld)
-
-    # Probando el metodo de reverse
-    ld.reverse()
-    print('Después de reverse():\n', ld)
-
-    # Probando el metodo de sort
-    ld.sort()
-    print('Después de sort():\n', ld)
     
-    # Probando el método __getitem__
-    elemento = ld[1]
-    print("Elemento en la posición 1:\n", elemento)
+    for i in range(10):
+        ld.append(i)
     
-    # Probando el método __contains__
-    print("¿El elemento 10 está en la lista?\n", 10 in ld)
-    
-    # Probando el método __eq__
-    otra_lista = ListaDinamica()
-    otra_lista.append(5)
-    otra_lista.append(15)
-    print("¿Las listas son iguales?\n", ld == otra_lista)
-    
-    # Probando el método __ne__
-    print("¿Las listas son diferentes?\n", ld != otra_lista)
-    
-    # Probando el método __delitem__
-    del ld[0]
-    print("Lista después de eliminar el primer elemento:\n", ld)
-    
-    # Probando el método __add__
-    nueva_lista = ld + otra_lista
-    print("Lista después de concatenarla con otra lista:\n", nueva_lista)
-    
-    # Probando el método __iadd__
-    ld += otra_lista
-    print("Lista después de concatenarla con otra lista (in-place):\n", ld)
-    
-    # Probando el método count
-    print("Número de veces que aparece el elemento 5:\n", ld.count(5))
-    
-    # Probando el método index
-    print("Índice de la primera aparición del elemento 15:\n", ld.index(15))
+    for j in range(10):
+        del ld[j]
